c2ce50c7fa76f8bd002047dc5cd35ad332e4fa4e
haseeb zafar <Haseebzafar107@gmail.com>
2020-04-06 22:49:58 +0500

    New commit

---
+++ app/src/main/java/com/example/covid_19_updates/CountryList.java
@@ -0,0 +1,123 @@
+package com.example.covid_19_updates;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import libs.mjn.prettydialog.PrettyDialog;
+import libs.mjn.prettydialog.PrettyDialogCallback;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class CountryList extends AppCompatActivity {
+
+    private RequestQueue mQueue;
+    private ListView listView;
+    private ArrayList<String> arrayList;
+    private ArrayAdapter arrayAdapter;
+    private String[] countryNames;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_country_list);
+
+        listView = (ListView) findViewById(R.id.theList);
+        arrayList = new ArrayList<String>();
+
+        arrayList.add("Hey");
+        arrayList.add("this");
+        arrayList.add("is");
+        arrayList.add("just");
+        arrayList.add("for");
+        arrayList.add("testing");
+        arrayAdapter = new ArrayAdapter(CountryList.this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(arrayAdapter);
+
+
+        // IGNORE ALL OF THIS
+        mQueue = Volley.newRequestQueue(this);
+        String url = "https://corona.lmao.ninja/countries";
+        //jsonArrReqString(url);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.menuSearchBtn);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setQueryHint("Type here to search...");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(final String newText) {
+                arrayAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return true;
+    }
+
+    private void jsonArrReqString(String url) {
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                int counter = 0;
+                for(int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject nestedObj = response.getJSONObject(i);
+                        countryNames [i] = nestedObj.getString("country");
+                        counter++;
+                        //arrayList.add(countryNames[i]);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                //arrayList.addAll(Arrays.asList(countryNames));
+
+                Toast.makeText(CountryList.this, arrayList.size() + "", Toast.LENGTH_SHORT).show();
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(CountryList.this, error.getMessage() + "", Toast.LENGTH_SHORT).show();
+            }
+        });
+        mQueue.add(request);
+    }
+
+}
--- app/src/main/java/com/example/covid_19_updates/MainActivity.java
+++ app/src/main/java/com/example/covid_19_updates/MainActivity.java
@@ -2,33 +2,94 @@ package com.example.covid_19_updates;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListView listView;
-    private ArrayList<String> arrayList;
-    private ArrayAdapter arrayAdapter;
+    private TextView totalCases, totalDeaths, totalRecovered, activeCases, affectedCountries;
+    private RequestQueue mQueue;
+    private ImageButton searchButton;
+
+    // Update able variables
+    String tCase, tDeath, tRec, active, affCountries;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] countryNames;
-        Resources res = getResources();
-        countryNames = res.getStringArray(R.array.countries_array);
+        totalCases = findViewById(R.id.totalCases);
+        totalDeaths = findViewById(R.id.totalDeaths);
+        totalRecovered = findViewById(R.id.totalRecovered);
+        activeCases = findViewById(R.id.activeCases);
+        affectedCountries = findViewById(R.id.affectedCountries);
 
-        listView = (ListView) findViewById(R.id.theList);
-        arrayList = new ArrayList<String>();
-        arrayList.addAll(Arrays.asList(countryNames));
-        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
-        listView.setAdapter(arrayAdapter);
+        searchButton = findViewById(R.id.searchButton);
+
+        mQueue = Volley.newRequestQueue(this);
+        jsonParse("https://corona.lmao.ninja/all");
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CountryList.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void jsonParse(String URL){
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, URL, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try{
+                            tCase = response.getString("cases");
+                            tDeath = response.getString("deaths");
+                            tRec = response.getString("recovered");
+                            active = response.getString("active");
+                            affCountries = response.getString("affectedCountries");
+                            // Updating
+                            totalCases.setText(tCase);
+                            totalDeaths.setText(tDeath);
+                            totalRecovered.setText(tRec);
+                            activeCases.setText(active);
+                            affectedCountries.setText(affCountries);
+
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        mQueue.add(request);
     }
 }
