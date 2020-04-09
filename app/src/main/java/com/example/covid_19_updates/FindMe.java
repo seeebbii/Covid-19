477fc595752c5fb2aa80fecb20541aa4a401751e
haseeb zafar <Haseebzafar107@gmail.com>
2020-04-09 23:15:18 +0500

    New commit

--- app/src/main/java/com/example/covid_19_updates/FindMe.java
+++ app/src/main/java/com/example/covid_19_updates/FindMe.java
@@ -1,11 +1,14 @@
 package com.example.covid_19_updates;
 
 import androidx.appcompat.app.AppCompatActivity;
+import libs.mjn.prettydialog.PrettyDialog;
+import libs.mjn.prettydialog.PrettyDialogCallback;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -36,6 +39,7 @@ public class FindMe extends AppCompatActivity {
     public Button correctBtn;
     public Button incorrectBtn;
     public ListView listView;
+    public TextView fetchedInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,6 +53,7 @@ public class FindMe extends AppCompatActivity {
         incorrectBtn = findViewById(R.id.incorrectBtn);
         countryName = findViewById(R.id.countryNameTxtView);
         generateBtn = findViewById(R.id.generateBtn);
+        fetchedInfo = findViewById(R.id.fetchedInfo);
         ipDisplay = findViewById(R.id.ipTxtView);
 
         final FetchDataClass fetchData = new FetchDataClass();
@@ -63,7 +68,6 @@ public class FindMe extends AppCompatActivity {
                     e.printStackTrace();
                 }
                 ipAdress = fetchData.getIp();
-                Toast.makeText(FindMe.this, ipAdress, Toast.LENGTH_LONG).show();
                // String url = "https://tools.keycdn.com/geo.json?host=";
                 parseJson("https://tools.keycdn.com/geo.json?host=", ipAdress);
 
@@ -77,7 +81,7 @@ public class FindMe extends AppCompatActivity {
                     public void onClick(View v) {
                         Toast.makeText(FindMe.this, "We are loading information...", Toast.LENGTH_LONG).show();
                         try {
-                            Thread.sleep(1000);
+                            Thread.sleep(100);
                         } catch (InterruptedException e) {
                             Toast.makeText(FindMe.this, e.getMessage(), Toast.LENGTH_LONG).show();
                         }
@@ -101,13 +105,37 @@ public class FindMe extends AppCompatActivity {
             }
         });
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                final PrettyDialog prettyDialog =  new PrettyDialog(FindMe.this);
+                prettyDialog.setTitle(adapter.getItem(position).getCountryname())
+                        .setMessage("Total Cases: " + adapter.getItem(position).getTotalcase() + "\n" + "Total Deaths: " + adapter.getItem(position).getTotaldeaths()
+                                + "\n" + "Active Cases: " + adapter.getItem(position).getActivecases() + "\n" + "Total Recovered: " + adapter.getItem(position).getTotalrecovered())
+                        .setIcon(R.drawable.searchbutton)
+                        .addButton(
+                                "OK",     // button text
+                                R.color.pdlg_color_white,  // button text color
+                                R.color.pdlg_color_green,  // button background color
+                                new PrettyDialogCallback() {  // button OnClick listener
+                                    @Override
+                                    public void onClick() {
+                                        // Do what you gotta do
+                                        prettyDialog.dismiss();
+                                    }
+                                }
+                        )
+                        .show();
+            }
+        });
+
 
     }
 
 
 
     private void parseJson(String url, String ip){
-        Toast.makeText(FindMe.this, "Please wait we are calculating..." ,Toast.LENGTH_LONG).show();
+        Toast.makeText(FindMe.this, "Please wait we are calculating..." ,Toast.LENGTH_SHORT).show();
 //        String finalUrl = url.concat(ip);
         //Toast.makeText(FindMe.this, url+ip ,Toast.LENGTH_LONG).show();
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url+ip, null, new Response.Listener<JSONObject>() {
@@ -138,9 +166,33 @@ public class FindMe extends AppCompatActivity {
         Thread.sleep(2000);
         try{
             //String finalUrl = url + countryName.getText().toString() +"?strict=true";
-            Toast.makeText(FindMe.this, url + countryName.getText().toString(), Toast.LENGTH_SHORT).show();
+            for(int i = 0; i < CountryList.c.length; i++){
+                if(countryName.getText().toString().equals(CountryList.c[i].getCountryname())){
+                    arrayList.add(CountryList.c[i]);
+                    fetchedInfo.setVisibility(View.VISIBLE);
+                }
+            }
+            adapter = new CountryAdapter(FindMe.this, arrayList);
+            listView.setAdapter(adapter);
         }catch (Exception e){
-            Toast.makeText(FindMe.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            final PrettyDialog prettyDialog =  new PrettyDialog(FindMe.this);
+            prettyDialog.setTitle("Stupid!")
+                    .setMessage("You have to search for your country at least once, don't waste my time...!")
+                    .setIcon(R.drawable.searchbutton)
+                    .addButton(
+                            "OK",     // button text
+                            R.color.pdlg_color_white,  // button text color
+                            R.color.pdlg_color_green,  // button background color
+                            new PrettyDialogCallback() {  // button OnClick listener
+                                @Override
+                                public void onClick() {
+                                    // Do what you gotta do
+                                    prettyDialog.dismiss();
+                                }
+                            }
+                    )
+                    .show();
+            //Toast.makeText(FindMe.this, "You have to search for your country once at least, don't waste my time...!", Toast.LENGTH_LONG).show();
         }
 
     }
